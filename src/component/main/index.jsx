aa94e3d8c5b8dd7d57def7128d597872e3622d98
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-09-01 08:05:13 +0900

    Animation

--- src/component/main/index.jsx
+++ src/component/main/index.jsx
@@ -1,13 +1,11 @@
 import * as S from './index.style'
 import image from './www.png'
 const Render = () => {
-    // const items = ['턱걸이', '딥스', '팔굽혀펴기', '윗몸일으키기', '맨몸 스쿼트', '버피', '플랭크', '브릿지', '마운틴 클라이머', '바이시클 메뉴버', '트릭킹']
     return (
         <S.Main>
             <S.mainTitle>Make Your Life Healthy</S.mainTitle>
             <S.subTitle>저희 사이트는 운동법을 보다 간편하고, 정확하게 제공합니다.</S.subTitle>
             <S.subTitle>사용자의 움직임을 체크하여, 잘못된 부분을 지적해줍니다.</S.subTitle>
-            <S.subTitle>ㅇㅇ</S.subTitle>
             <S.startButton>시작하기</S.startButton>
         </S.Main>
     )
--- src/component/main/index.style.jsx
+++ src/component/main/index.style.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 export const Main = styled.div`
     width: 100vw;
     display: flex;
@@ -8,16 +8,41 @@ export const Main = styled.div`
     margin-top: 200px;
     margin-bottom: 100px;
     box-sizing: border-box;
+    `
+const titleAnimation = keyframes`
+    0% { 
+        transform: translate(-1000, 0);
+    }
+    100% {
+        transform: translate(0, 0);
+    }
 `
+const buttonAnimation = keyframes`
+    0% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+    `
 export const mainTitle = styled.span`
     font-size: 55px;
     margin-left: 40px;
     margin-bottom: 40px;
-`
+    transform: translate(-1000px, 0);
+    animation-duration: 1s;
+    animation-fill-mode: forwards;
+    animation-name: ${titleAnimation};
+    `
 export const subTitle = styled.span`
     font-size: 25px;
     margin-left: 60px;
     margin-bottom: 5px;
+    transform: translate(-1000px, 0);
+    animation-duration: 1s;
+    animation-delay: 0.8s;
+    animation-fill-mode: forwards;
+    animation-name: ${titleAnimation};
 `
 export const startButton = styled.div`
     display: flex;
@@ -25,61 +50,21 @@ export const startButton = styled.div`
     height: 50px;
     justify-content: center;
     align-items: center;
+    font-size: 20px;
     margin-top: 50px;
-    margin-left: auto;
+    margin-left: 70px;
     margin-right: auto;
     color: #eee;
     background-color: gray;
     border-radius: 5px;
+    opacity: 0;
     cursor: pointer;
+    animation: ${buttonAnimation};
+    animation-delay: 1.8s;
+    animation-duration: 0.7s;
+    animation-fill-mode: forwards;
+    transition: 0.15s ease-in-out;
+    &:hover {
+        opacity: 0.8
+    }
 `
-
-// export const MainContent = styled.div`
-//     display: flex;
-//     width: 1000px;
-//     height: 300px;
-//     background-color: lightgray;
-//     border: 4px solid black;
-//     border-radius: 5px;
-//     margin-bottom: 50px;
-//     box-sizing: border-box;
-// `
-
-// export const MyRecord = styled.div`
-//     display: flex;
-//     width: 80vw;
-//     height: 200px;
-//     border: 4px solid black;
-//     border-radius: 5px;
-//     margin-bottom: 50px;
-//     box-sizing: border-box;
-// `
-
-// export const TrainingList = styled.div`
-//     display: flex;
-//     flex-wrap: wrap;
-//     justify-content: space-between;
-//     width: 450px;
-//     row-gap: 20px;
-//     box-sizing: border-box;
-// `
-
-// export const TestBox = styled.div`
-//     display: flex;
-//     width: 100px;
-//     height: 100px;
-//     justify-content: center;
-//     align-items: center;
-//     border: 1px solid black;
-//     word-break: keep-all;
-//     transition: .15s ease-in-out;
-//     box-sizing: border-box;
-//     cursor: pointer;
-//     &:hover {
-//         box-shadow: 3px 3px 3px grey;
-//     }
-//     > span {
-//         text-align: center;
-//         font-size: 16px;
-//     }
-// `
