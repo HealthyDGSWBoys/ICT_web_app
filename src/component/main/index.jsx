5638368f282159b28f337dfcb13e2cba98b79a8c
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-08-31 16:41:13 +0900

    Update index.jsx

--- src/component/main/index.jsx
+++ src/component/main/index.jsx
@@ -7,6 +7,7 @@ const Render = () => {
             <S.mainTitle>Make Your Life Healthy</S.mainTitle>
             <S.subTitle>저희 사이트는 운동법을 보다 간편하고, 정확하게 제공합니다.</S.subTitle>
             <S.subTitle>사용자의 움직임을 체크하여, 잘못된 부분을 지적해줍니다.</S.subTitle>
+            <S.subTitle>ㅇㅇ</S.subTitle>
             <S.startButton>시작하기</S.startButton>
         </S.Main>
     )
