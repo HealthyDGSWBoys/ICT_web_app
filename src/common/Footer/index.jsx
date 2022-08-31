95ca93d3dc026fb8d64ab23ba7ba0d62811bebab
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-08-31 20:17:31 +0900

    [add]

--- src/common/Footer/index.jsx
+++ src/common/Footer/index.jsx
@@ -1,10 +1,33 @@
-import * as S from './index.style'
+import { useState } from 'react'
+import * as I from './index.style'
+
+import footerImg from './footerImg.png';
+
 const Render = () => {
+    const[footerLink, setFooterLink] = useState([
+        {
+            name: 'home', link: ''
+        },
+        {
+            name: 'introduce', link: 'introduce'
+        },
+        {
+            name: 'recommend', link: 'recommend'
+        }
+    ])
     return (
-        <S.Main>
-            푸터 입니다
-        </S.Main>
+        <I.Main>
+            <I.FooterImgPart src={footerImg}></I.FooterImgPart>
+            {
+                footerLink.map((a, i)=>{
+                    return(
+                        <I.A to={a.link}>{a.name}</I.A>
+                    )
+                })
+            }
+        </I.Main>
     )
+
 }
 
 export default Render
--- src/common/Footer/index.style.jsx
+++ src/common/Footer/index.style.jsx
@@ -1,7 +1,27 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 export const Main = styled.div`
   width: 100vw;
-  height: 140px;
+  height: 100px;
   background-color: grey;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: solid 2px black;
+`
+
+export const A = styled(Link)`
+  margin-left: 25px;
+  margin-right: 25px;
+  text-decoration: none;
+  &:hover{
+    text-decoration:underline;
+  }
+`
+
+export const FooterImgPart = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 100px;
 `
