45f00b4f6a87aa425ec045629d00d00fd0cb6305
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-08-31 16:40:35 +0900

    h

--- src/component/main/index.jsx
+++ src/component/main/index.jsx
@@ -1,17 +1,13 @@
 import * as S from './index.style'
 import image from './www.png'
 const Render = () => {
-    const items = ['턱걸이', '딥스', '팔굽혀펴기', '윗몸일으키기', '맨몸 스쿼트', '버피', '플랭크', '브릿지', '마운틴 클라이머', '바이시클 메뉴버', '트릭킹']
+    // const items = ['턱걸이', '딥스', '팔굽혀펴기', '윗몸일으키기', '맨몸 스쿼트', '버피', '플랭크', '브릿지', '마운틴 클라이머', '바이시클 메뉴버', '트릭킹']
     return (
         <S.Main>
-            {/* <img src={image} width='200'></img> */}
-            <S.MainContent></S.MainContent>
-            <S.MyRecord></S.MyRecord>
-            <S.TrainingList>
-                {
-                    items.map(item => (<S.TestBox><span>{item}</span></S.TestBox>))
-                }
-            </S.TrainingList>
+            <S.mainTitle>Make Your Life Healthy</S.mainTitle>
+            <S.subTitle>저희 사이트는 운동법을 보다 간편하고, 정확하게 제공합니다.</S.subTitle>
+            <S.subTitle>사용자의 움직임을 체크하여, 잘못된 부분을 지적해줍니다.</S.subTitle>
+            <S.startButton>시작하기</S.startButton>
         </S.Main>
     )
 }
--- src/component/main/index.style.jsx
+++ src/component/main/index.style.jsx
@@ -3,59 +3,83 @@ export const Main = styled.div`
     width: 100vw;
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: left;
     flex-direction: column;
-    margin-top: 100px;
+    margin-top: 200px;
     margin-bottom: 100px;
     box-sizing: border-box;
 `
-
-export const MainContent = styled.div`
-    display: flex;
-    width: 1000px;
-    height: 300px;
-    background-color: lightgray;
-    border: 4px solid black;
-    border-radius: 5px;
-    margin-bottom: 50px;
-    box-sizing: border-box;
+export const mainTitle = styled.span`
+    font-size: 55px;
+    margin-left: 40px;
+    margin-bottom: 40px;
 `
-
-export const MyRecord = styled.div`
-    display: flex;
-    width: 80vw;
-    height: 200px;
-    border: 4px solid black;
-    border-radius: 5px;
-    margin-bottom: 50px;
-    box-sizing: border-box;
+export const subTitle = styled.span`
+    font-size: 25px;
+    margin-left: 60px;
+    margin-bottom: 5px;
 `
-
-export const TrainingList = styled.div`
+export const startButton = styled.div`
     display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    width: 450px;
-    row-gap: 20px;
-    box-sizing: border-box;
-`
-
-export const TestBox = styled.div`
-    display: flex;
-    width: 100px;
-    height: 100px;
+    width: 200px;
+    height: 50px;
     justify-content: center;
     align-items: center;
-    border: 1px solid black;
-    word-break: keep-all;
-    transition: .15s ease-in-out;
-    box-sizing: border-box;
+    margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    color: #eee;
+    background-color: gray;
+    border-radius: 5px;
     cursor: pointer;
-    &:hover {
-        box-shadow: 3px 3px 3px grey;
-    }
-    > span {
-        text-align: center;
-        font-size: 16px;
-    }
 `
+
+// export const MainContent = styled.div`
+//     display: flex;
+//     width: 1000px;
+//     height: 300px;
+//     background-color: lightgray;
+//     border: 4px solid black;
+//     border-radius: 5px;
+//     margin-bottom: 50px;
+//     box-sizing: border-box;
+// `
+
+// export const MyRecord = styled.div`
+//     display: flex;
+//     width: 80vw;
+//     height: 200px;
+//     border: 4px solid black;
+//     border-radius: 5px;
+//     margin-bottom: 50px;
+//     box-sizing: border-box;
+// `
+
+// export const TrainingList = styled.div`
+//     display: flex;
+//     flex-wrap: wrap;
+//     justify-content: space-between;
+//     width: 450px;
+//     row-gap: 20px;
+//     box-sizing: border-box;
+// `
+
+// export const TestBox = styled.div`
+//     display: flex;
+//     width: 100px;
+//     height: 100px;
+//     justify-content: center;
+//     align-items: center;
+//     border: 1px solid black;
+//     word-break: keep-all;
+//     transition: .15s ease-in-out;
+//     box-sizing: border-box;
+//     cursor: pointer;
+//     &:hover {
+//         box-shadow: 3px 3px 3px grey;
+//     }
+//     > span {
+//         text-align: center;
+//         font-size: 16px;
+//     }
+// `
