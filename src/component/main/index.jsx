f85afb209c6a945c8978fffcca3083b16669a190
종박 <78944379+jongpark1234@users.noreply.github.com>
2022-09-07 18:15:08 +0900

    Main Update

--- src/component/main/index.jsx
+++ src/component/main/index.jsx
@@ -1,12 +1,13 @@
 import * as S from './index.style'
-import image from './www.png'
 const Render = () => {
     return (
         <S.Main>
-            <S.mainTitle>Make Your Life Healthy</S.mainTitle>
-            <S.subTitle>저희 사이트는 운동법을 보다 간편하고, 정확하게 제공합니다.</S.subTitle>
-            <S.subTitle>사용자의 움직임을 체크하여, 잘못된 부분을 지적해줍니다.</S.subTitle>
-            <S.startButton>시작하기</S.startButton>
+            <S.textBox>
+                <S.mainTitle>Make Your Life Healthy</S.mainTitle>
+                <S.subTitle>저희 사이트는 운동법을 보다 간편하고, 정확하게 제공합니다.</S.subTitle>
+                <S.subTitle>사용자의 움직임을 체크하여, 잘못된 부분을 지적해줍니다.</S.subTitle>
+                <S.startButton>시작하기</S.startButton>
+            </S.textBox>
         </S.Main>
     )
 }
--- src/component/main/index.style.jsx
+++ src/component/main/index.style.jsx
@@ -1,14 +1,5 @@
 import styled, { keyframes } from "styled-components";
-export const Main = styled.div`
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: left;
-    flex-direction: column;
-    margin-top: 200px;
-    margin-bottom: 100px;
-    box-sizing: border-box;
-    `
+import background from './bg.jpg'
 const titleAnimation = keyframes`
     0% { 
         transform: translate(-1000, 0);
@@ -19,25 +10,52 @@ const titleAnimation = keyframes`
 `
 const buttonAnimation = keyframes`
     0% {
+        transform: translate(0, 0);
         opacity: 0;
     }
     100% {
+        transform: translate(0, 0);
         opacity: 1;
     }
-    `
+`
+export const Main = styled.div`
+    width: 100vw;
+    height: 1280px;
+    display: flex;
+    align-items: left;
+    flex-direction: column;
+    box-sizing: border-box;
+
+    /* filter: brightness(50%); */
+    background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.4)), url(${background});
+    background-size: cover;
+`
+export const BackgroundImage = styled.img`
+    width: 100vw;
+    height: 100vh;
+`
+export const textBox = styled.div`
+    display: flex;
+    margin-top: 15%;
+    justify-content: center;
+    align-items: left;
+    flex-direction: column;
+`
 export const mainTitle = styled.span`
     font-size: 55px;
     margin-left: 40px;
     margin-bottom: 40px;
+    color: #dadada;
     transform: translate(-1000px, 0);
     animation-duration: 1s;
     animation-fill-mode: forwards;
     animation-name: ${titleAnimation};
-    `
+`
 export const subTitle = styled.span`
     font-size: 25px;
     margin-left: 60px;
     margin-bottom: 5px;
+    color: #dadada;
     transform: translate(-1000px, 0);
     animation-duration: 1s;
     animation-delay: 0.8s;
@@ -58,6 +76,7 @@ export const startButton = styled.div`
     background-color: gray;
     border-radius: 5px;
     opacity: 0;
+    transform: translate(-1000px, 0);
     cursor: pointer;
     animation: ${buttonAnimation};
     animation-delay: 1.8s;
