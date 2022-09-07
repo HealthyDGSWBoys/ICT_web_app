500c6edb626ed9b39f949ee0e9474fc4a43d14bd
오은지 <81677536+oheunji05@users.noreply.github.com>
2022-09-07 17:44:44 +0900

    [add]

--- src/common/Footer/index.jsx
+++ src/common/Footer/index.jsx
@@ -2,29 +2,45 @@ import { useState } from 'react'
 import * as I from './index.style'
 
 import footerImg from './footerImg.png';
+import schoolImg from './schoolImg.png';
+import DUCAMIImg from './DUCAMIImg.png';
 
 const Render = () => {
     const[footerLink, setFooterLink] = useState([
         {
-            name: 'home', link: ''
+            name: '홈', link: ''
         },
         {
             name: 'introduce', link: 'introduce'
         },
         {
             name: 'recommend', link: 'recommend'
+        },
+        {
+            name: 'record', link: 'record'
         }
     ])
     return (
         <I.Main>
-            <I.FooterImgPart src={footerImg}></I.FooterImgPart>
-            {
-                footerLink.map((a, i)=>{
-                    return(
-                        <I.A to={a.link}>{a.name}</I.A>
-                    )
-                })
-            }
+
+            <I.Linked>
+                <I.FooterImgPart src={footerImg}></I.FooterImgPart>
+                {
+                    footerLink.map((a, i)=>{
+                        return(
+                            <I.A to={a.link}>{a.name}</I.A>
+                        )
+                    })
+                }
+            </I.Linked>
+
+            <I.IntroduceBy>
+                <img src={schoolImg} width="23px" height="20px"></img>
+                <I.SchoolText>대구소프트웨어마이스터고등학교</I.SchoolText>
+                {/* <img src={DUCAMIImg} width="17px" height="20px"></img> */}
+                <I.DucamiText>DUCAMI</I.DucamiText>
+            </I.IntroduceBy>
+            
         </I.Main>
     )
 
--- src/common/Footer/index.style.jsx
+++ src/common/Footer/index.style.jsx
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom';
 
 export const Main = styled.div`
   width: 100vw;
-  height: 100px;
-  background-color: grey;
+  height: 150px;
+  background-color: black;
+  //border-top: solid 2px black;
+`
+
+export const Linked = styled.div`
+  width: 100%;
+  height: 90px;
   display: flex;
   align-items: center;
   justify-content: center;
-  border-top: solid 2px black;
+  border-bottom: solid 1px white;
 `
 
 export const A = styled(Link)`
@@ -18,10 +24,31 @@ export const A = styled(Link)`
   &:hover{
     text-decoration:underline;
   }
+  font-weight: bold;
+  color: white;
 `
 
 export const FooterImgPart = styled.img`
-  width: 30px;
-  height: 30px;
-  margin-right: 100px;
+  width: 40px;
+  height: 40px;
+  margin-right: 25px;
+`
+
+export const IntroduceBy = styled.div`
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const SchoolText = styled.div`
+  font-size: 15px;
+  margin-left: 40px;
+  margin-right: 40px;
+  color: white;
+`
+
+export const DucamiText = styled.div`
+  font-size: 15px;
+  color: white;
 `
