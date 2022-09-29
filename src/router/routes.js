9e658c8501a751ecda4c1778b9716f666aef535c
chldkduds2 <101560578+chldkduds2@users.noreply.github.com>
2022-09-29 23:38:33 +0900

    setting router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,13 @@
 import Main from "../components/main";
 import Test from "../components/test";
 import NotFound from "../common/NotFound";
+import Login from "../components/Login/Login";
+import Join from "../components/Join/Join";
 
 export default [
   { path: "", component: <Main />, nav: true, footer: true },
   { path: "*", component: <NotFound />, nav: false, footer: false },
   { path: "test", component: <Test />, nav: true, footer: true },
+  { path:"join", component:<Join />, nav: false, footer:false },
+  {path:"login",component: <Login />, nav: false, footer:false}
 ]
