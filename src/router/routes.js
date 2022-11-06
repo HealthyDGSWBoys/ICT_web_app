400844771b4d4c75295bc4624b6f0b9e15845ee3
kim geon ho <80884472+qfourca@users.noreply.github.com>
2022-11-06 21:02:19 +0900

    b

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,10 +5,9 @@ import Join from "../components/Join/Join";
 import Home from '../components/Home/Home';
 
 export default [
-  { path: "", component:  <Home />, nav: false, footer: false },
-  { path: "/", component:  <Home />, nav: false, footer: false },
+  { path: "ICT_web_app", component:  <Home />, nav: false, footer: false },
   { path: "*", component: <NotFound />, nav: false, footer: false },
-  { path: "test", component: <Test />, nav: false, footer: true },
-  { path:"Join", component:<Join />, nav: false, footer:false },
-  { path:"Login",component: <Login />, nav: false, footer:false},
+  { path: "ICT_web_app/test", component: <Test />, nav: false, footer: true },
+  { path:"ICT_web_app/Join", component:<Join />, nav: false, footer:false },
+  { path:"ICT_web_app/Login",component: <Login />, nav: false, footer:false},
 ]
